826fc04e417f5fb308cd125b0759cc77fd99c8f2
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-09-11 13:14:51 +0800

    update koa

--- packages/nsfw/src/nsfw.ts
+++ packages/nsfw/src/nsfw.ts
@@ -1,6 +1,7 @@
 import { load, NSFWJS } from './lib/nsfwjs'
 import * as tf from '@tensorflow/tfjs'
 import sharp from 'sharp'
+import crypto from 'crypto'
 import { IMAGE_RESIZE } from '../config'
 
 let model: NSFWJS
@@ -9,6 +10,22 @@ export async function loadModel() {
   if (!model) {
     model = await load()
   }
+}
+
+// 图片哈希字典
+const imageHashDict: Record<string, Record<string, number>> = {}
+
+// 将 tf.Tensor3D 转换为普通的 JavaScript 数组，并计算哈希值
+function tensorToHash(tensor: tf.Tensor3D): string {
+  const data = tensor.dataSync()
+  const shape = tensor.shape
+  const flatArray = Array.from(
+    { length: shape[0] * shape[1] * shape[2] },
+    (_, index) => data[index]
+  )
+  const hash = crypto.createHash('sha256')
+  hash.update(flatArray.join('')) // 将数组拼接成一个字符串并计算哈希值
+  return hash.digest('hex')
 }
 
 async function loadImageAndConvert(imageUrl: string) {
@@ -27,7 +44,7 @@ async function loadImageAndConvert(imageUrl: string) {
 
   // 创建一个 Uint8Array 来存储图像的像素数据
   const pixelData = new Uint8Array(await sharp(imageBuffer).resize(reWidth, reHeight).toBuffer())
-  console.log('pixelData', pixelData)
+  // console.log('pixelData', pixelData)
   // 将图像转换为 Tensor3D
   return tf.browser.fromPixels({ width: reWidth, height: reHeight, data: pixelData })
 }
@@ -35,7 +52,16 @@ async function loadImageAndConvert(imageUrl: string) {
 export async function detectImage(imageUrl: string) {
   console.log('detectImage', imageUrl)
   const imageData = await loadImageAndConvert(imageUrl)
-  console.log('imageData', imageData)
+  // console.log('imageData', imageData)
+  // 计算图像的哈希值
+  const imageHash = tensorToHash(imageData)
+  console.log('imageHash', imageHash)
+  if (imageHashDict[imageHash]) {
+    // 如果图像已经被分类过，就直接返回之前的分类结果
+    console.log('Image already classified')
+    return imageHashDict[imageHash]
+  }
+
   // 使用 nsfwjs 进行分类并计算推理时间
   const startTime = Date.now()
   const predictions = await model.classify(imageData)
@@ -47,5 +73,9 @@ export async function detectImage(imageUrl: string) {
     rating[predictions.className] = predictions.probability
   })
   console.log('rating', rating)
+
+  // 将分类结果存储到哈希字典中
+  imageHashDict[imageHash] = rating
+
   return rating
 }
