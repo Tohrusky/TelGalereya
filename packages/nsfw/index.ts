1e055e1095776901c92150fadcb8c0ac3cdebd34
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-09-11 16:22:03 +0800

    update nsfw

--- packages/nsfw/index.ts
+++ packages/nsfw/index.ts
@@ -3,15 +3,6 @@ import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import { detectImage, loadModel } from './src/nsfw'
 
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
-) {
-  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
-    process.env.LD_LIBRARY_PATH || ''
-  }`
-}
-
 const app = new Koa()
 const router = new Router()
 
