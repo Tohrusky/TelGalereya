1c679ede9dffb9787b8116e53628ad5338199d65
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-09-11 17:35:50 +0800

    feat image nsfw detector

---
+++ packages/tgimg-worker/config.ts
@@ -0,0 +1,5 @@
+export const NSFW_DETECTOR = true
+
+export const NSFW_API_URL = 'https://tgimg-worker.vercel.app/api/v1/nsfw-check'
+
+export const NSFW_DEFAULT_IMAGE = 'https://telegra.ph/file/fb57cac5517050c4726d2.jpg'
--- packages/tgimg-worker/src/services/image.ts
+++ packages/tgimg-worker/src/services/image.ts
@@ -1,9 +1,34 @@
 import { IRequest } from 'itty-router/Router'
 import { Base64 } from 'js-base64'
+import { NSFW_API_URL, NSFW_DEFAULT_IMAGE, NSFW_DETECTOR } from '../../config.ts'
+import { NSFWResponseType } from '@tgimg-worker/nsfw/src/types.ts'
 
 export async function handleImage(request: IRequest) {
   const { params } = request
-  const url = 'https://telegra.ph/file/' + Base64.decode(params.id)
+  const tgBaseUrl = 'https://telegra.ph'
+  const url = tgBaseUrl + '/file/' + Base64.decode(params.id)
 
-  return fetch(url)
+  if (!NSFW_DETECTOR) {
+    return await fetch(url)
+  }
+
+  try {
+    const response = await fetch(NSFW_API_URL + '?url=' + url)
+
+    const res: NSFWResponseType = await response.json()
+    console.log(res)
+    if (res?.status === 'success') {
+      if (res.nsfw) {
+        return await fetch(NSFW_DEFAULT_IMAGE)
+      } else {
+        return await fetch(url)
+      }
+    }
+
+    return await fetch(url)
+  } catch (err) {
+    console.error(err)
+    // Handle error and return an appropriate response
+    return await fetch(url)
+  }
 }
